a68b4fe8ff664e685aa64dc6b9c14a0b53942e8a
arthurDiff <arthur.lee945@gmail.com>
2025-04-26 21:41:34 -0500

    making pytorch work here is a nightmare

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -3,10 +3,10 @@ pub struct Analyzer {}
 impl Analyzer {
     pub fn new() -> Self{
         // let file_path = std::path::absolute("./models/model.pt").unwrap();
-        let file_path = std::path::absolute("./models/full_image_88545560_22x8_v12_epoch_00153.pt").unwrap();
+        let file_path = std::path::absolute("./models/md_v5a.0.0.pt").unwrap();
         println!("{:?}", &file_path);
         let md = tch::CModule::load(file_path);
-        println!("{:?}", md);
+        println!("{:#?}", md);
         Self{}
     }
 }
